d29cd6f257866472368550dd46aa178fc06af49d
jpedrosantosb <jpedrosantosb@gmail.com>
2023-07-13 16:33:48 -0300

    Renderizando os cards

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -7,22 +8,41 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca = ''
   subscription: Subscription
+  livro: Livro
 
   constructor(private service: LivroService) { }
 
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe(
       {
-        next: retornoAPI => console.log(),
-        error: erro => console.error(erro),
-        complete: () => console.log('Observable completado!')
+        next: (items) => {
+          this.listaLivros = this.livrosResultadoParaLivros(items)
+        },
+        error: erro => console.error(erro)
       }
     )
+  }
+
+  livrosResultadoParaLivros(items) {
+    const livros: Livro[] = []
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    })
+
+    return livros
   }
 
   ngOnDestroy() {
